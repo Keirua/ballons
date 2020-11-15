5482b691281c8aac2bf7c24c7c83d66a1fa491f7
clemkeirua <clement@keiruaprod.fr>
2020-11-15 20:24:22 +0100

    possibility to configure the settings via CLI

--- src/bin/multiple_games.rs
+++ src/bin/multiple_games.rs
@@ -2,17 +2,40 @@ extern crate ballons;
 use ballons::game::BalloonGame;
 use std::collections::HashMap;
 
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(about = "Running multiple game iterations.")]
+struct Opt {
+    /// number of players
+    #[structopt(short = "n", long = "nb_players", default_value = "2")]
+    nb_players: usize,
+
+    /// number of cards per player
+    #[structopt(short = "c", long = "nb_cards", default_value = "5")]
+    nb_cards: usize,
+
+    /// number of parent cards
+    #[structopt(short = "p", long = "nb_parents", default_value = "5")]
+    nb_parents: usize,
+
+    /// number of iteration
+    #[structopt(short = "i", long = "nb_iterations", default_value = "100")]
+    nb_iterations: usize,
+}
+
 fn main() {
-	let nb_games = 100;
-	let mut frequencies:HashMap<usize, usize> = HashMap::new();
+    let opt = Opt::from_args();
 
-	for _ in 0..nb_games {
-    	let mut balloon_game = BalloonGame::new(2, 5, 5);
-    	let nb_cards = balloon_game.run_game();
+    let mut frequencies: HashMap<usize, usize> = HashMap::new();
 
-    	let entry = frequencies.entry(nb_cards).or_insert(0);
-    	*entry += 1;
-	}
+    for _ in 0..opt.nb_iterations {
+        let mut balloon_game = BalloonGame::new(opt.nb_players, opt.nb_cards, opt.nb_parents);
+        let nb_cards = balloon_game.run_game();
+
+        let entry = frequencies.entry(nb_cards).or_insert(0);
+        *entry += 1;
+    }
 
     println!("{:?}", frequencies);
 }
