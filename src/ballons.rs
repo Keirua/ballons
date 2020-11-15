766b09dea9da34a77cc65e860e0027ac3e55e99e
clemkeirua <clement@keiruaprod.fr>
2020-11-15 10:12:23 +0100

    split files

---
+++ src/ballons.rs
@@ -0,0 +1,18 @@
+#[derive(Debug, PartialEq, Eq, Hash, Copy, Clone)]
+pub enum Balloons {
+	Red,
+	Blue,
+	Green,
+	Violet,
+	Yellow
+}
+
+#[derive(Debug, PartialEq, Eq, Hash, Copy, Clone)]
+pub enum ActionCard {
+	Red,
+	Blue,
+	Green,
+	Violet,
+	Yellow,
+	Parent
+}
