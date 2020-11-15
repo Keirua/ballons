b2a7614fdf4cc74d187dbbe021a1c5b27f8e573f
clemkeirua <clement@keiruaprod.fr>
2020-11-15 09:46:45 +0100

    added deal and burst card

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 
-#[derive(Debug, PartialEq, Eq, Hash)]
+#[derive(Debug, PartialEq, Eq, Hash, Copy, Clone)]
 enum Balloons {
 	Red,
 	Blue,
@@ -9,7 +9,7 @@ enum Balloons {
 	Yellow
 }
 
-#[derive(Debug, PartialEq, Eq, Hash)]
+#[derive(Debug, PartialEq, Eq, Hash, Copy, Clone)]
 enum ActionCard {
 	Red,
 	Blue,
@@ -21,13 +21,15 @@ enum ActionCard {
 
 #[derive(Debug)]
 struct Player {
-	ballons: HashMap<Balloons, usize>
+	ballons: HashMap<Balloons, usize>,
+	returned_balloons: Vec<Balloons>
 }
 
 impl Player {
 	pub fn new() -> Player {
 		let mut p = Player {
-			ballons: HashMap::new()
+			ballons: HashMap::new(),
+			returned_balloons: Vec::new()
 		};
 		p.ballons.insert(Balloons::Red, 0);
 		p.ballons.insert(Balloons::Blue, 0);
@@ -36,10 +38,26 @@ impl Player {
 		p.ballons.insert(Balloons::Yellow, 0);
 		p
 	}
+
+	pub fn deal(&mut self, b: Balloons) {
+		let count = self.ballons.entry(b).or_insert(0);
+		*count += 1;
+	}
+
+	pub fn burst(&mut self, b: Balloons) {
+		let count = self.ballons.entry(b).or_insert(0);
+		if *count > 0 {
+			*count -= 1;
+			self.returned_balloons.push(b);
+		}
+	}
 }
 
 fn main() {
-	let p = Player::new();
+	let mut p = Player::new();
+	p.deal(Balloons::Red);
+	p.burst(Balloons::Blue);
+	p.burst(Balloons::Red);
 
     println!("{:?}", p);
 }
