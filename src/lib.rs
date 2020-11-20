13703b5da84304dad0e3adcfd6cb8bc4d7b5e9e7
clemkeirua <clement@keiruaprod.fr>
2020-11-20 16:15:36 +0100

    added a rust function that exposes running a game

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,12 @@ mod ballons;
 mod deck;
 pub mod game;
 mod player;
+
+
+use crate::game::BalloonGame;
+
+#[no_mangle]
+pub extern "C" fn run_balloon_game(nb_players: usize, nb_cards: usize, nb_parents: usize) -> usize {
+  let mut balloon_game = BalloonGame::new(nb_players, nb_cards, nb_parents);
+  balloon_game.run_game()
+}
