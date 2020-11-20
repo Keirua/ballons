229c066445fedebf96d5f1d4b42f3eb092559b42
clemkeirua <clement@keiruaprod.fr>
2020-11-20 11:33:42 +0100

    generate all possible hands

--- gen_every_hands.py
+++ gen_every_hands.py
@@ -18,28 +18,41 @@ def gen5():
 	hands = [ [c]*5 for c in colors()]
 	return hands
 
-def gen41():
+def gen_two_colors_hand(nb_c1, nb_c2):
 	hands = []
-	for c4 in colors():
-		for c1 in colors()-set([c4]):
-			hands.append([c4]*4 + [c1])
+	for c1 in colors():
+		for c2 in colors()-set([c1]):
+			hands.append([c1]*nb_c1 + [c2]*nb_c2)
 	return hands
 
+def gen41():
+	return gen_two_colors_hand(4, 1)
+
 def gen32():
+	return gen_two_colors_hand(3, 2)
+
+def gen_three_colors_hand(nb_c1, nb_c2, nb_c3):
 	hands = []
-	for c3 in colors():
-		for c2 in colors()-set([c3]):
-			hands.append([c3]*3 + [c2]*2)
+	for c1 in colors():
+		for c2 in colors()-set([c1]):
+			for c3 in colors()-set([c1])-set([c2]):
+				hands.append([c1]*nb_c1 + [c2]*nb_c2 + [c3]*nb_c3)
 	return hands
 
 def gen311():
-	pass
+	return gen_three_colors_hand(3, 1, 1)
 
 def gen221():
-	pass
+	return gen_three_colors_hand(2, 2, 1)
 
 def gen2111():
-	pass
+	hands = []
+	for c1 in colors():
+		for c2 in colors()-set([c1]):
+			for c3 in colors()-set([c1])-set([c2]):
+				for c4 in colors()-set([c1])-set([c2])-set([c3]):
+					hands.append([c1]*2 + [c2, c3, c4])
+	return hands
 
 
 print("11111")
@@ -56,4 +69,16 @@ print("")
 
 print("32")
 print(gen32())
+print("")
+
+print("311")
+print(gen311())
+print("")
+
+print("221")
+print(gen221())
+print("")
+
+print("2111")
+print(gen2111())
 print("")
